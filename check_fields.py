c69138737eb67024a8f7cd4106ba9f5b1b3cef60
arnaud <arnaud@pixeeplay.com>
2026-02-12 10:13:48 +0100

    test

---
+++ check_fields.py
@@ -0,0 +1,5 @@
+import xmlrpc.client
+url = 'https://nus-odoo.productsmanager.app'
+db = 'nus-odoo'
+username = 'admin' # Assuming admin, but might need to check
+password = '...' # I don't have the password, so I can't use this directly.
